4c1b0692c38795e362cd6775c0fff1d12b9737e6
Pavel <sir.buzmakoff@yandex.ru>
2025-06-18 10:11:51 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <h1>This is Ebite Techologies</h1>
+      <button>Let's Ebite this world with us!</button>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // Делает сервер доступным по IP-адресу
+    port:3000
+  },
+})
